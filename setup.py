fce94c01b1d848e5c2a32fad8b6136c29adb0412
Gaston Michel <michel.z.gaston@gmail.com>
2022-08-07 01:50:22 -0300

    test automation

--- setup.py
+++ setup.py
@@ -1,17 +1,14 @@
-import os
 from setuptools import setup, find_packages
 from os import environ
 
-url = '{GITHUB_SERVER_URL}/{GITHUB_REPOSITORY}'
-version = environ['GITHUB_REF'].split('/')[-1]
 
 setup(
     name='afip-ws',
-    version=version,
+    version=environ['GITHUB_REF_NAME'],
     author='Gaston Michel',
     author_email='michel.z.gaston@gmail.com',
     description='Python library to easily interact with AFIP Web Services',
-    url=url.format(**environ),
+    url='{GITHUB_SERVER_URL}/{GITHUB_REPOSITORY}'.format(**environ),
     long_description_content_type='text/markdown',
     long_description=open('README.md').read(),
     packages=find_packages('./afip'),
